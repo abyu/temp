6b9ebba95c6e56436c089af65fe4ef0475e129fa
Kishore <kishore_kumar-tw@external.mckinsey.com>
2015-07-05 15:34:13 +0530

    New steam game model, write to game_data db

--- downloadinfo.rb
+++ downloadinfo.rb
@@ -3,19 +3,6 @@ require 'net/http'
 require 'json'
 require 'yaml'
 
-class SteamGameInfoSchema
-  def keys_to_include 
-    ["type", "name", "steam_appid", "required_age", "is_free",
-    "about_the_game", "supported_languages", "header_image", "website", "platforms", "developers", "publishers", "price_overview", "packages", 
-    "platforms", "metacritic", "categories", "genres", "recommendations",
-    "release_date"]
-  end
-
-  def parse(json_data)
-    json_data.reject {|k,v| !keys_to_include.include?(k)}
-  end
-end
-
 class GameInformationGateway 
 
   def download(appid)
@@ -24,35 +11,72 @@ class GameInformationGateway
     resp = Net::HTTP.get(URI.parse(url))
 
     resp
-  end
+  end 
 end
   
 class Game < CouchRest::Model::Base
 
-  property :name,	String
   property :source,	String
-  property :data, 	String
 
   timestamps!
 
-end
-
-File.open("allappids").each do |id|
-  appid = id.strip
-  begin
-    game_data_raw = JSON.parse(GameInformationGateway.new.download(appid))
-    steam_game_info_parser = SteamGameInfoSchema.new
-    if game_data_raw[appid.to_s]["success"]
-      game_data = game_data_raw[appid.to_s]["data"]
-      game = Game.new(:name => game_data["name"], :source => "steam", :data => steam_game_info_parser.parse(game_data))
-      game.save
-
-    else
-      p "no success"
-    end    
-  rescue Exception => e
-    p e
+  design do
+    view :all
   end
 
 end
+
+class SteamGame < Game
+
+  SCHEMA_KEYS = ["name", "steam_appid", "required_age", "is_free",
+    "about_the_game", "supported_languages", "header_image", "website", "platforms",
+    "developers", "publishers", "price_overview", "background", 
+    "platforms", "metacritic", "categories", "genres", "recommendations",
+    "release_date", "movies"]
+
+  SCHEMA_KEYS_SPECIAL = {"type": "steam_type"}  
+
+  self.class_eval do
+    SCHEMA_KEYS.concat(SCHEMA_KEYS_SPECIAL.values).each do |key|
+      property key.to_sym
+    end
+  end
+
+  def initialize(opts = {})
+    
+    SCHEMA_KEYS.each do |key|
+      send("#{key}=", opts[key])
+    end
+    SCHEMA_KEYS_SPECIAL.each do |k, v|
+      send("#{v}=", opts[k.to_s])
+    end
+    send("source=", "steam")
+  end
+end
+
+def download_game_info
+  info_gateway = GameInformationGateway.new
+  error_log = File.open("error.log", "w")
+  File.open("allappids").each do |id|
+    appid = id.strip
+    begin
+      game_data_raw = JSON.parse(info_gateway.download(appid))
+      get_game_info(game_data_raw, appid, error_log)
+    rescue Exception => e
+      error_log.write("Exception: Failed to download app data for appid: #{appid}, with exception: #{exception}\n")
+    end
+  end
+end
+
+def get_game_info(game_data_raw, appid, log)
+  if game_data_raw[appid.to_s]["success"]
+    game_data = game_data_raw[appid.to_s]["data"]
+    game = SteamGame.new(game_data)
+    game.save
+  else
+    log.write("No data available for appid: #{appid}\n")
+  end
+end
+
+download_game_info
 
